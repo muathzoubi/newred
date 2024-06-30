c97f041bffad7e34a6dd096c923c08108d868f6c
muath alzoubi <32450805+muathzoubi@users.noreply.github.com>
2024-06-30 20:44:02 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import "./App.css";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
+
+const UrgeWithPleasureComponent = () => (
+  <CountdownCircleTimer
+    isPlaying
+    duration={7}
+    size={100}
+    colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
+    colorsTime={[7, 5, 2, 0]}
+  >
+    {({ remainingTime }) => remainingTime}
+  </CountdownCircleTimer>
+);
+function App() {
+  return (
+    <>
+      <div className="countdown">
+        <UrgeWithPleasureComponent />
+      </div>
+      <p>
+        سوف يتم اعادة توجيهك تلقائياً للموقع بعد 7 ثواني او اضغط على الأنتقال
+        للموقع
+      </p>
+
+      <div className="card">
+        <a href="#">
+          <button>الأنتقال للموقع</button>
+        </a>
+      </div>
+      <p className="read-the-docs">
+        قد تستغرق هذه العملية وقتاً اكثر من اللازم
+      </p>
+    </>
+  );
+}
+
+export default App;
