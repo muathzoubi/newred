d55049c7673b570d8ee98fa8487efca821aa0dc6
muath alzoubi <32450805+muathzoubi@users.noreply.github.com>
2024-07-01 19:46:25 +0000

    njml

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
       </p>
 
       <div className="card">
-        <a href="#">
+        <a href="https://www.invsment-opo.com/">
           <button>الأنتقال للموقع</button>
         </a>
       </div>
